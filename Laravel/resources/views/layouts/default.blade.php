0951bcc2ebe4bbd23c082eda4abdeafec5ddb8e1
xiyangwushang <376567702@qq.com>
2019-11-08 02:19:20 +0800

    Enter

--- Laravel/resources/views/layouts/default.blade.php
+++ Laravel/resources/views/layouts/default.blade.php
@@ -5,18 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>this is public event @yield('tittle','开始')</title>
-    <link rel="stylesheet" href="/Laravel/public/css/welcome.css">
-    <link rel="stylesheet" href="{{mix('css/app.css')}}">
+    
+    <link rel="stylesheet" href="/css/app.css">
+    <!-- 
+        <link rel="stylesheet" href=""> 
+    -->
 </head>
 <body>
-    <nav class="navber navbar-dark bg-dark navbar-expand-lg">
-        <div>
-            <a class="navber_brand" href="/">welcome</a>
-            <ul class="nav-item justify-content-end">
-                <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
-                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
-            </ul>
-        </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container">
+                  <a class="navbar-brand" href="/">Weibo App</a>
+                  <ul class="navbar-nav justify-content-end">
+                    <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+                    <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+                  </ul>
+                </div>
+              </nav>
     <div id="content" name="content">       
         @yield('content')    
     </div>
