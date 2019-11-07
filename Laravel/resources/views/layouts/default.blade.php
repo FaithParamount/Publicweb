401bbc404789e56f7089ac5902b45012c675aa83
xiyangwushang <376567702@qq.com>
2019-11-08 01:37:35 +0800

    Enter

--- Laravel/resources/views/layouts/default.blade.php
+++ Laravel/resources/views/layouts/default.blade.php
@@ -5,10 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>this is public event @yield('tittle','开始')</title>
+    <link rel="stylesheet" href="/Laravel/public/css/welcome.css">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
 <body>
-    <button>1</button>
-    @yield('content')
-    <button>2</button>
+    <nav class="navber navbar-dark bg-dark navbar-expand-lg">
+        <div>
+            <a class="navber_brand" href="/">welcome</a>
+            <ul class="nav-item justify-content-end">
+                <li class="nav-item"><a href="/help" class="nav-link">帮助</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">登录</a></li>
+            </ul>
+        </div>
+    <div id="content" name="content">       
+        @yield('content')    
+    </div>
 </body>
 </html>
