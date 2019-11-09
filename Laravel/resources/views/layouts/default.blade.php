7b7252bff9b7bf88eb29058d2063e45e971662b1
xiyangwushang <376567702@qq.com>
2019-11-10 01:09:29 +0800

    Enter

--- Laravel/resources/views/layouts/default.blade.php
+++ Laravel/resources/views/layouts/default.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>this is public event @yield('tittle','开始')</title>
     
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{mix('css/app.css')}}">
     <!-- 
         <link rel="stylesheet" href=""> 
     -->
