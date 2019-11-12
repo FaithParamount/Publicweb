4ec1e7c685c25e2324d27893e2f0b7fd240206a6
xiyangwushang <376567702@qq.com>
2019-11-13 00:51:33 +0800

    Enter

--- Laravel/resources/views/static_pages/about.blade.php
+++ Laravel/resources/views/static_pages/about.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.default')
 @section('content')
-    <button>about</button>
+    <button>本人与二零一九年十一月六号立项 处于毕业后五个月在家玩的飞起 实属闲的蛋疼 创立此项目用于学习php laravel linux</button>
 @stop
 @section('tittle','关于')
+<!-- 有没有一起玩彩六的 星际2的 全境封锁2 命运2 钢铁雄心 水深火热洛圣都 白给精英 野蛮5 野蛮6 群星 无尽空间2 崩坏三的 -->
+<!-- upaly:Faith_paramount    steam:beachsaber -->
