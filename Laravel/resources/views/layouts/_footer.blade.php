09fd380f98db3aa5a9ee786d78974c4d862a95ec
xiyangwushang <376567702@qq.com>
2019-11-13 01:13:37 +0800

    Enter

--- Laravel/resources/views/layouts/_footer.blade.php
+++ Laravel/resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <img src="">
-    <a href="www.baidu.com">本人已单方面于百度达成战略合作协议 不知道的请百度</a>
+    <a href="www.baidu.com">提示百度</a>
     <div class="float-right">
-    <a href="/about">关于this</a>
+    <a href="/about" target=_blank>关于this</a>
 </footer>
