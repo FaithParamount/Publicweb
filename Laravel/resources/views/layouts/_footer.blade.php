74cba7f4917a41e7da7fa389dece65f9740697f3
xiyangwushang <376567702@qq.com>
2019-11-13 03:20:45 +0800

    Enter

--- Laravel/resources/views/layouts/_footer.blade.php
+++ Laravel/resources/views/layouts/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer">
     <img src="">
-    <a href="www.baidu.com">提示百度</a>
+    <a href="http://www.baidu.com">Everything is inside</a>
     <div class="float-right">
     <a href="{{ route('about') }}" target=_blank>关于this</a>
 </footer>
